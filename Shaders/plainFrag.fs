06817b0e60528639caac6091ca77ea03dd0d5e93
P17215040 <P17215040@my365.dmu.ac.uk>
2020-03-02 16:02:27 +0000

    Working 3

--- Shaders/plainFrag.fs
+++ Shaders/plainFrag.fs
@@ -4,8 +4,18 @@ out vec4 FragColor;
 in vec2 TexCoords;
 in vec3 FragPos;
 
+//uniform vec3 lightPos;
+//uniform vec3 eyePos;
+
 void main()
 {
+    //vec3 lightDir   = normalize(lightPos - FragPos);
+    //vec3 viewDir    = normalize(eyePos - FragPos);
+    //vec3 halfwayDir = normalize(lightDir + viewDir);
+
+    //float spec = pow(max(dot(normal, halfwayDir), 0.0), shininess);
+    //vec3 specular = lightColor * spec;
+
     FragColor = vec4(0.2,0.8,0.3,1.0);
 }
 	
